fe9899fa8daad7644ef76ba2bbfe6b809873c6dc
Dan Damian <arcassis@gmail.com>
2024-10-10 12:14:28 +0300

    Lib.rs is working.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use std::env;
-use std::fmt::Error;
-use std::fs;
 use std::process;
+use minigrep::Config;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,20 +8,11 @@ fn main() {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
-}
+    println!("Searching for '{}'", config.query);
+    println!("In file '{}'", config.file_path);
 
-struct Config {
-    query: String,
-    file_path: String
-}
-
-impl Config {
-    pub fn build(args: &[String]) -> Result<Self, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments")
-        }
-        let query = args[1].clone();
-        let file_path = args[2].clone();
-        Ok(Config{ query, file_path })
+    if let Err(e) = minigrep::run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 }
